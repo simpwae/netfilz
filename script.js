d75167d31b601c4b2e5ca3173f5738050587fad4
simpwae <144361530+simpwae@users.noreply.github.com>
2024-01-17 20:14:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// let firstWord = 10;
+// let secondWord = 20;
+// let Result = firstWord < secondWord;
+// console.log(Result);
+
+
+
+// let score = prompt ("please enter your score")
+// if (score <0 || score  > 100)
+// {
+//     console.log("invalid")
+// }
+// else if (score >= 80 && score <= 90)
+// {
+//     console.log("A")
+// }
+// else if (score >= 70 && score <= 80)
+// {
+//     console.log("B")
+// }
+// else if ( score >= 60 && score <= 70)
+// {
+//     console.log("C")
+// }
+// else if (score >= 50 && score <= 60)
+// {
+//     console.log("D")
+// }
+// else {
+//     console.log ("F")
+// }
+
+
+let hight = prompt ("Enter Your Hight :")
+let speed = prompt ( " Enter You Speed :")
+{ 
+if (hight >=5.9 && speed >= 150)
+ console.log("You Are A Fast Bowler")
+}
+{
+else if  (hight >= 5.5 && speed >= 130)
+console.log ( "You Are A Swing Bowler")
+}
+{
+else (hight <=5.5 && speed <= 130)
+console.log ( " You Are Invilad ")
+}
